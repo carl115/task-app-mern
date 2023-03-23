6cdf646308037469a92a42d99358c8291f49d611
Carl <cfds2003@gmail.com>
2023-03-23 18:18:30 -0500

    backend modified

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,19 @@
-import express from 'express'
-import tasks from './routes/tasks.routes'
-import connectDB from './db/db'
+import express from "express";
+import tasks from "./routes/tasks.routes";
+import connectDB from "./db/db";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
-app.set('port', 3000)
+app.set("port", 3000);
 
-connectDB()
+connectDB();
 
-app.use('/api/tasks', tasks)
+app.use("/api/tasks", tasks);
 
-app.listen(app.get('port'), () => console.log(`Server listening on port ${app.get('port')}`))
+app.listen(app.get("port"), () =>
+  console.log(`Server listening on port ${app.get("port")}`)
+);
