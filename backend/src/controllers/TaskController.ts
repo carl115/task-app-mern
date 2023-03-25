a6b621d0b1765363675a034a2372aa72a1ead0a3
Carl <cfds2003@gmail.com>
2023-03-25 13:34:46 -0500

    little changes in requests

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -1,53 +1,58 @@
-import { Task } from '../db/models/Task'
-import { TaskRequest } from '../types'
-import { requestTitle, requestBody } from '../helpers/TaskRequest'
+import { Task } from "../db/models/Task";
+import { TaskRequest, TaskData } from "../types";
+import {
+  requestTitle,
+  requestBody,
+  requestCheck,
+} from "../helpers/TaskRequest";
 
-export const getTasks = async (_req:any, res:any) => {
-    const taskFind = await Task.find()
+export const getTasks = async (_req: any, res: any) => {
+  const taskFind = await Task.find();
 
-    res.json(taskFind)
-}
+  res.json(taskFind);
+};
 
-export const getTask = async (req:any, res:any) => {
-    const taskFind = await Task.findById(req.params.id)
+export const getTask = async (req: any, res: any) => {
+  const taskFind = await Task.findById(req.params.id);
 
-    res.json(taskFind)
-}
+  res.json(taskFind);
+};
 
-export const createTask = async (req:any, res:any) => {
-    try {
-        const insertTask:TaskRequest = {
-            title: requestTitle(req.body.title),
-            body: requestBody(req.body.body)
-        }
+export const createTask = async (req: any, res: any) => {
+  try {
+    const insertTask: TaskRequest = {
+      title: requestTitle(req.body.title),
+      body: requestBody(req.body.body),
+    };
 
-        await Task.create(insertTask)
+    await Task.create(insertTask);
 
-        res.json({ message: 'Task created successfully' })
-    } catch (error) {
-        console.error(error)
-        res.status(400).json({ messageError: 'Error creating task' })
-    }
-}
+    res.json({ message: "Task created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ messageError: "Error creating task" });
+  }
+};
 
-export const updateTask = async (req:any, res:any) => {
-    try {
-        const insertTask:TaskRequest = {
-            title: requestTitle(req.body.title),
-            body: requestBody(req.body.body)
-        }
+export const updateTask = async (req: any, res: any) => {
+  try {
+    const insertTask: TaskData = {
+      title: requestTitle(req.body.title),
+      body: requestBody(req.body.body),
+      check: requestCheck(req.body.check),
+    };
 
-        await Task.findByIdAndUpdate(req.params.id, insertTask)
+    await Task.findByIdAndUpdate(req.params.id, insertTask);
 
-        res.json({ message: 'Task updated successfully' })
-    } catch (error) {
-        console.error(error)
-        res.status(400).json({ messageError: 'Error updating task' })
-    }
-}
+    res.json({ message: "Task updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ messageError: "Error updating task" });
+  }
+};
 
-export const deleteTask = async (req:any, res:any) => {
-    await Task.findByIdAndRemove(req.params.id)
+export const deleteTask = async (req: any, res: any) => {
+  await Task.findByIdAndRemove(req.params.id);
 
-    res.sendStatus(204)
-}
+  res.sendStatus(204);
+};
--- backend/src/helpers/TaskRequest.ts
+++ backend/src/helpers/TaskRequest.ts
@@ -1,19 +1,31 @@
-export const requestTitle = (field:string) => {
-    if(!isString(field)) {
-        throw new Error('Title is not valid')
-    }
+export const requestTitle = (field: string) => {
+  if (!isString(field)) {
+    throw new Error("Title is not valid");
+  }
 
-    return field
-}
+  return field;
+};
 
-export const requestBody = (field:string) => {
-    if(!isString(field)) {
-        throw new Error('Body is not valid')
-    }
+export const requestBody = (field: string) => {
+  if (!isString(field)) {
+    throw new Error("Body is not valid");
+  }
 
-    return field
-}
+  return field;
+};
 
-const isString = (param:string) => {
-    return typeof(param) === 'string' ? true : false
-}
+export const requestCheck = (field: boolean) => {
+  if (!isBoolean(field)) {
+    throw new Error("Check is not valid");
+  }
+
+  return field;
+};
+
+const isString = (param: string) => {
+  return typeof param === "string" ? true : false;
+};
+
+const isBoolean = (param: boolean) => {
+  return typeof param === "boolean" ? true : false;
+};
